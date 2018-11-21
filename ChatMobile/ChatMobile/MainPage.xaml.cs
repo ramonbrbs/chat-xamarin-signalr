f0a2bcc8ea5e395c17f3d83a4d1e003c6cf2d239
ramonbrbs <ramonflmr@gmail.com>
2018-11-21 14:23:41 -0300

    Creates application Mobile

---
+++ ChatMobile/ChatMobile/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ChatMobile
+{
+    
+    public class ChatMessage: INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+    }
+
+    public class ChatVM: INotifyPropertyChanged
+    {
+
+        public string Name { get; set; }
+        public string Message { get; set; }
+        
+
+
+        private HubConnection Connection;
+        private IHubProxy ChatHubProxy;
+        public delegate void MessageReceived(string name, string message);
+        public event MessageReceived OnMessageReceived;
+
+        public Command SendCmd { get; set; }
+
+
+
+        public ObservableCollection<ChatMessage> Messages { get; set; }
+        public string ConnectionState { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ChatVM(string url)
+        {
+        
+            SendCmd = new Command(() =>
+            {
+                if (String.IsNullOrEmpty(Name) || String.IsNullOrEmpty(Message))
+                {
+                    return;
+                }
+                ChatHubProxy.Invoke("ClientToServer", Name, Message);
+            });
+
+            try
+            {
+                Messages = new ObservableCollection<ChatMessage>();
+                Connection = new HubConnection(url);
+                Connection.StateChanged += (StateChange obj) =>
+                {
+                    ConnectionState = obj.NewState.ToString();
+                };
+                OnMessageReceived += (name, message) =>
+                {
+                    Messages.Add(new ChatMessage(){Content = message,Name = name});
+                };
+
+                ChatHubProxy = Connection.CreateHubProxy("ChatHub");
+                ChatHubProxy.On<string, string>("ServerToClient", (username, text) => {
+                    OnMessageReceived?.Invoke(username, text);
+                });
+                Connection.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+
+    }
+
+
+    public partial class MainPage : ContentPage
+    {
+
+        public const string IP = "10.0.0.67";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new ChatVM($"http://{IP}/ChatWeb");
+        }
+    }
+}
--- ChatWeb/Hubs/ChatHub.cs
+++ ChatWeb/Hubs/ChatHub.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.AspNet.SignalR;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace ChatWeb.Hubs
 {
+    [HubName("ChatHub")]
     public class ChatHub : Hub
     {
         public void ClientToServer(string user, string message)
